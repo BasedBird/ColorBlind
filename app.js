368160814ed11e02ef4f08849b1e939c8df483cb
Sophia Zhou <slzhou73@gmail.com>
2020-10-10 20:35:21 -0400

    testing out different branches

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//hello there
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
@@ -10,11 +11,63 @@ app.use('/client',express.static(__dirname + '/client'));
 serv.listen(8000);
 console.log('Server started.');
 
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id){
+  var self = {
+    x:250,
+    y:250,
+    id:id,
+    number:"" + Math.floor(10*Math.random()),
+    presstingLeft:false,
+    pressingDown: false,
+    pressingRight: false,
+    pressingUp: false,
+    maxSpd:10
+  }
+  return self;
+}
+
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection',function(socket){
-  console.log('socket connection');
+  socket.id = Math.random();
+  SOCKET_LIST[socket.id] = socket;
 
-  socket.on('happy',function(){
-    console.log('happy');
+  var player = Player(socket.id);
+  PLAYER_LIST[socket.id] = player;
+
+  socket.on('disconnect',function(){
+    delete SOCKET_LIST[socket.id];
+    delete PLAYER_LIST[socket.id];
+  });
+
+  socket.on('keyPress',function(data){
+    if(data.inputId === 'left')
+      player.pressingLeft = data.state;
+    else if(data.inputId === 'right')
+      player.pressingRight = data.state;
+    else if(data.inputId === 'down')
+      player.pressingDown = data.state;
+    else if(data.inputId === 'up')
+      player.pressingUp = data.state;
   });
 });
+
+setInterval(function (){
+  var pack = [];
+  for(var i in PLAYER_LIST){
+    var player = PLAYER_LIST[i];
+    //player.x++;
+    //player.y++;
+    pack.push({
+      x:player.x,
+      y:player.y,
+      number:player.number
+    });
+  }
+  for(var i in SOCKET_LIST){
+    var socket = SOCKET_LIST[i];
+    socket.emit('newPositions',pack);
+  }
+},1000/25);
